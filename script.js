83398f5e1574702488a961e37d54716270e966e2
Glitch (hello-webpage) <none>
2021-05-25 15:09:02 +0000

    🚴🍋 Checkpoint ./script.js:32166188/2837

--- script.js
+++ script.js
@@ -1,32 +1,33 @@
-const video = document.getElementById('webcam');
-const liveView = document.getElementById('liveView');
-const demosSection = document.getElementById('demos');
-const enableWebcamButton = document.getElementById('webcamButton');
+const video = document.getElementById("webcam");
+const liveView = document.getElementById("liveView");
+const demosSection = document.getElementById("demos");
+const enableWebcamButton = document.getElementById("webcamButton");
 
 // Check if webcam access is supported.
 function getUserMediaSupported() {
-  return !!(navigator.mediaDevices &&
-    navigator.mediaDevices.getUserMedia);
+  return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 }
 
 // If webcam supported, add event listener to button for when user
-// wants to activate it to call enableCam function which we will 
+// wants to activate it to call enableCam function which we will
 // define in the next step.
 if (getUserMediaSupported()) {
-  enableWebcamButton.addEventListener('click', enableCam);
+  enableWebcamButton.addEventListener("click", enableCam);
 } else {
-  console.warn('getUserMedia() is not supported by your browser');
+  console.warn("getUserMedia() is not supported by your browser");
 }
+
+function enableCam(event) {}
 
 function enableCam(event) {
   // Only continue if the COCO-SSD has finished loading.
   if (!model) {
     return;
   }
-  
+
   // Hide the button once clicked.
-  event.target.classList.add('removed');  
-  
+  event.target.classList.add("removed");
+
   // getUsermedia parameters to force video but not audio.
   const constraints = {
     video: true
@@ -35,12 +36,11 @@ function enableCam(event) {
   // Activate the webcam stream.
   navigator.mediaDevices.getUserMedia(constraints).then(function(stream) {
     video.srcObject = stream;
-    video.addEventListener('loadeddata', predictWebcam);
+    video.addEventListener("loadeddata", predictWebcam);
   });
 }
-function predictWebcam() {
-}
+function predictWebcam() {}
 
 // Pretend model has loaded so we can try out the webcam code.
 var model = true;
-demosSection.classList.remove('invisible');
+demosSection.classList.remove("invisible");
