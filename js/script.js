7b01c0f3bd3bd122b7f16ae8f5a142dc5e1ff0b5
Glitch (hello-webpage) <none>
2021-05-25 21:56:53 +0530

    final commit before uploading

--- js/script.js
+++ js/script.js
@@ -8,9 +8,7 @@ function getUserMediaSupported() {
   return !!(navigator.mediaDevices && navigator.mediaDevices.getUserMedia);
 }
 
-// If webcam supported, add event listener to button for when user
-// wants to activate it to call enableCam function which we will
-// define in the next step.
+
 if (getUserMediaSupported()) {
   enableWebcamButton.addEventListener("click", enableCam);
 } else {
@@ -42,30 +40,22 @@ function enableCam(event) {
 
 var model = undefined;
 
-// Before we can use COCO-SSD class we must wait for it to finish
-// loading. Machine Learning models can be large and take a moment 
-// to get everything needed to run.
-// Note: cocoSsd is an external object loaded from our index.html
-// script tag import so ignore any warning in Glitch.
+
 cocoSsd.load().then(function (loadedModel) {
   model = loadedModel;
-  // Show demo section now model is ready to use.
   demosSection.classList.remove('invisible');
 });
 
 var children = [];
 
 function predictWebcam() {
-  // Now let's start classifying a frame in the stream.
+  
   model.detect(video).then(function (predictions) {
-    // Remove any highlighting we did previous frame.
     for (let i = 0; i < children.length; i++) {
       liveView.removeChild(children[i]);
     }
     children.splice(0);
     
-    // Now lets loop through predictions and draw them to the live view if
-    // they have a high confidence score.
     for (let n = 0; n < predictions.length; n++) {
       // If we are over 66% sure we are sure we classified it right, draw it!
       if (predictions[n].score > 0.66) {
