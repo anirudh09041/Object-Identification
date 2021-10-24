122916f7bcdcbf11ef65b4413127e55edc34550c
nilesh-dhangar20 <nilesh45155@gmail.com>
2021-10-24 17:32:28 +0530

    adding file to anirudh

---
+++ LinkedList/Runner.java
@@ -0,0 +1,17 @@
+package LinkedList;
+
+
+
+public class Runner {
+
+    
+    public static void main(String[] args) {
+      Implementation list=new Implementation();
+        list.insertList(1);
+        list.insertList(2);
+        list.insertList(3);
+        list.insertList(4);
+        list.insertList(5);
+        list.display();
+    }
+}
